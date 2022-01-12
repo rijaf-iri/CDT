524ec3392c3332ee68a271ef45ce4b916f7c240c
rijaf-iri <rijaf@iri.columbia.edu>
2022-01-12 01:45:43 +0000

    spliting & combining netCDF data

--- R/cdtCombineNetCDF_Procs.R
+++ R/cdtCombineNetCDF_Procs.R
@@ -13,19 +13,85 @@ combine.netcdf_writeNC <- function(){
     }
 
     dateOpts <- switch(GalParams$tstep,
-                       'minute' = list(nc = 5, fun = as.POSIXct, format = "%Y-%m-%d-%H-%M"),
-                       'hourly' = list(nc = 4, fun = as.POSIXct, format = "%Y-%m-%d-%H"),
-                       'daily' = list(nc = 3, fun = as.Date, format = "%Y-%m-%d"),
-                       'pentad' = list(nc = 3, fun = as.Date, format = "%Y-%m-%d"),
-                       'dekadal' = list(nc = 3, fun = as.Date, format = "%Y-%m-%d"),
-                       'monthly' = list(nc = 3, fun = as.Date, format = "%Y-%m-%d"),
+                       'minute' = list(nc = 5, fun = as.POSIXct,
+                                       format = "%Y-%m-%d-%H-%M",
+                                       units = "seconds since 1970-01-01 00:00:00"),
+                       'hourly' = list(nc = 4, fun = as.POSIXct,
+                                       format = "%Y-%m-%d-%H", 
+                                       units = "seconds since 1970-01-01 00:00:00"),
+                       'daily' = list(nc = 3, fun = as.Date,
+                                      format = "%Y-%m-%d",
+                                      units = "days since 1970-01-01"),
+                       'pentad' = list(nc = 3, fun = as.Date,
+                                       format = "%Y-%m-%d",
+                                       units = "days since 1970-01-01"),
+                       'dekadal' = list(nc = 3, fun = as.Date,
+                                        format = "%Y-%m-%d",
+                                        units = "days since 1970-01-01"),
+                       'monthly' = list(nc = 3, fun = as.Date,
+                                        format = "%Y-%m-%d",
+                                        units = "days since 1970-01-01")
                       )
 
     datetimes <- apply(rdate0[, 1:dateOpts$nc], 1, paste, collapse = "-")
     datetimes <- dateOpts$fun(datetimes, format = dateOpts$format)
+    datetimes <- as.numeric(datetimes)
 
+    dtimes <- rdate[, -ncol(rdate), drop = FALSE]
+    dtimes <- split(dtimes, col(dtimes))
+    ncfiles <- do.call(sprintf, c(list(fmt = GalParams$ncdf$format), dtimes))
+    dtimes <- do.call(paste0, dtimes)
+
+    ncpath <- file.path(GalParams$ncdf$dir, ncfiles)
+    existFl <- unlist(lapply(ncpath, file.exists))
+
+    if(!any(existFl)){
+        Insert.Messages.Out(GalParams[['message']][['4']], TRUE, "e")
+        return(NULL)
+    }
+
+    datetimes <- datetimes[existFl]
+    ncpath <- ncpath[existFl]
+    dtimes <- dtimes[existFl]
 
     ##################
+
+    ncinfo <- getNCDFSampleData(GalParams$ncdf$sample)
+    if(is.null(ncinfo)){
+        Insert.Messages.Out(GalParams[['message']][['5']], TRUE, "e")
+        return(NULL)
+    }
+
+    nlon <- length(ncinfo$lon)
+    nlat <- length(ncinfo$lat)
+
+    dx <- ncdf4::ncdim_def("lon", "degreeE", ncinfo$lon, longname = "Longitude")
+    dy <- ncdf4::ncdim_def("lat", "degreeN", ncinfo$lat, longname = "Latitude")
+    dt <- ncdf4::ncdim_def("time", dateOpts$units, datetimes,
+                           calendar =  "standard", longname = "Time")
+
+    ncgrd <- ncdf4::ncvar_def(ncinfo$varinfo$name, ncinfo$varinfo$units, list(dx, dy, dt),
+                              ncinfo$varinfo$missval, ncinfo$varinfo$longname,
+                              ncinfo$varinfo$prec, compression = 6)
+
+    ncout <- ncdf4::nc_create(GalParams$file2save, ncgrd)
+
+    for(j in seq_along(ncpath)){
+        nc <- ncdf4::nc_open(ncpath[j])
+        zval <- ncdf4::ncvar_get(nc, varid = ncinfo$varid)
+        ncdf4::nc_close(nc)
+
+        zval <- transposeNCDFData(zval, ncinfo)
+        zval[is.na(zval)] <- ncinfo$varinfo$missval
+
+        ncdf4::ncvar_put(ncout, ncgrd, zval, start = c(1, 1, j), count = c(nlon, nlat, 1))
+    }
+
+    ncdf4::ncatt_put(ncout, "lon", "axis", "X")
+    ncdf4::ncatt_put(ncout, "lat", "axis", "Y")
+    ncdf4::ncatt_put(ncout, "time", "axis", "T")
+    ncdf4::ncatt_put(ncout, 0, "title", ncinfo$varinfo$longname)
+    ncdf4::nc_close(ncout)
 
     return(0)
 }
--- R/cdtSplit3DNetCDF_Procs.R
+++ R/cdtSplit3DNetCDF_Procs.R
@@ -69,6 +69,36 @@ get.netcdf.time <- function(time.dim){
     c(time.dim[c('name', 'len')], list(origin = origin, units = units, vals = daty))
 }
 
+format.netcdf.time <- function(ncpars, time.dim){
+    time.name <- which(ncpars$var.dim == ncpars$dim[[3]]$name)
+    ret <- get.netcdf.time(time.dim)
+    daty <- ret$vals
+    if(!is.null(ret$origin)){
+        if(ret$len > 1){
+            rdaty <- lapply(c("%Y", "%m", "%d", "%H", "%M", "%S"),
+                            function(v) format(daty, v))
+            for(it in 6:1){
+                if(all.equal.elements(rdaty[[it]]))
+                   rdaty[[it]] <- NULL
+               else
+                    break
+            }
+            daty <- do.call(paste0, rdaty)
+        }else{
+            frmt <- switch(ret$units,
+                           "seconds" = "%Y%m%d%H%M%S",
+                           "minutes" = "%Y%m%d%H%M",
+                           "hours" = "%Y%m%d%H",
+                           "days" = "%Y%m%d",
+                           "months" = "%Y%m",
+                                   "%Y%m%d%H%M%S")
+            daty <- format(daty, frmt)
+        }
+    }
+
+    return(daty)
+}
+
 ###################################################################
 
 split_3d.netcdf_writeNC <- function(){
@@ -88,71 +118,68 @@ split_3d.netcdf_writeNC <- function(){
         ncfiles <- basename(.cdtData$GalParams$ncdf$file)
     }
 
+    ##################
+
+    dx <- do.call(ncdf4::ncdim_def, 
+                    c(ncpars$dim[[1]][c('name', 'units', 'vals')],
+                      list(longname = "Longitude")))
+    dy <- do.call(ncdf4::ncdim_def,
+                    c(ncpars$dim[[2]][c('name', 'units', 'vals')],
+                      list(longname = "Latitude")))
+    outnc <- ncdf4::ncvar_def(ncpars$var$name, ncpars$var$units,
+                              list(dx, dy), -99999, ncpars$var$longname,
+                              ncpars$var$prec, compression = 9)
+
+    ##################
+
+    nlon <- ncpars$dim[[1]]$len
+    nlat <- ncpars$dim[[2]]$len
+    start <- rep(NA, ncpars$var$ndims)
+    count <- rep(NA, ncpars$var$ndims)
+
+    posx <- which(ncpars$var.dim == ncpars$dim[[1]]$name)
+    posy <- which(ncpars$var.dim == ncpars$dim[[2]]$name)
+    post <- which(ncpars$var.dim == ncpars$dim[[3]]$name)
+
+    start[posx] <- 1
+    start[posy] <- 1
+    count[posx] <- nlon
+    count[posy] <- nlat
+    count[post] <- 1
+
+    if(ncpars$var$ndims > 3){
+        for(j in 4:ncpars$var$ndims){
+            pos <- which(ncpars$var.dim == ncpars$dim[[j]]$name)
+            start[pos] <- ncpars$dim[[j]]$sel
+            count[pos] <- 1
+        }
+    }
+
+    ##################
+
     ret <- lapply(seq_along(ncfiles), function(jj){
-        nc <- nc_open(file.path(nc.dir, ncfiles[jj]))
-        time.dim <- nc$dim[[ncpars$dim[[3]]$name]]
-        var.val <- ncvar_get(nc, varid = ncpars$var$name, collapse_degen = FALSE)
-        nc_close(nc)
-
-        if((ncpars$var$ndims - 3) > 0){
-            indices <- lapply(1:(ncpars$var$ndims - 3), function(i){
-                vdim <- ncpars$dim[[i + 3]]
-                list(name = which(ncpars$var.dim == vdim$name), ix = vdim$sel)
-            })
-            noms <- sapply(indices, "[[", "name")
-            indices <- lapply(indices, "[[", "ix")
-            names(indices) <- noms
-            var.val <- R.utils::extract(var.val, indices = indices, drop = TRUE)
-            ncpars$var.dim <- ncpars$var.dim[-noms]
-        }
-
-        time.name <- which(ncpars$var.dim == ncpars$dim[[3]]$name)
-        ret <- get.netcdf.time(time.dim)
-        daty <- ret$vals
-        if(!is.null(ret$origin)){
-            if(ret$len > 1){
-                rdaty <- lapply(c("%Y", "%m", "%d", "%H", "%M", "%S"), function(v) format(daty, v))
-                for(it in 6:1){
-                    if(all.equal.elements(rdaty[[it]]))
-                       rdaty[[it]] <- NULL
-                   else
-                        break
-                }
-                daty <- do.call(paste0, rdaty)
-            }else{
-                frmt <- switch(ret$units,
-                               "seconds" = "%Y%m%d%H%M%S",
-                               "minutes" = "%Y%m%d%H%M",
-                               "hours" = "%Y%m%d%H",
-                               "days" = "%Y%m%d",
-                               "months" = "%Y%m",
-                                       "%Y%m%d%H%M%S")
-                daty <- format(daty, frmt)
-            }
-        }
-
-        dx <- do.call(ncdim_def, c(ncpars$dim[[1]][c('name', 'units', 'vals')],
-                                   list(longname = "Longitude")))
-        dy <- do.call(ncdim_def, c(ncpars$dim[[2]][c('name', 'units', 'vals')],
-                                   list(longname = "Latitude")))
-        outnc <- ncvar_def(ncpars$var$name, ncpars$var$units, list(dx, dy), -9999,
-                           ncpars$var$longname, ncpars$var$prec, compression = 9)
+        ncin <- ncdf4::nc_open(file.path(nc.dir, ncfiles[jj]))
+        time.dim <- ncin$dim[[ncpars$dim[[3]]$name]]
+        daty <- format.netcdf.time(ncpars, time.dim)
 
         for(i in seq_along(daty)){
-            indices <- list(i)
-            names(indices) <- time.name
-            tmp <- R.utils::extract(var.val, indices = indices, drop = TRUE)
+            start[post] <- i
+            tmp <- ncdf4::ncvar_get(ncin, varid = ncpars$var$name,
+                                    start = start, count = count,
+                                    collapse_degen = TRUE)
+
             if(ncpars$rev) tmp <- t(tmp)
             tmp <- tmp[ncpars$dim[[1]]$order, ncpars$dim[[2]]$order]
-            tmp[is.na(tmp)] <- -9999
+            tmp[is.na(tmp)] <- -99999
 
             ncfile <- paste0(ncpars$var$name, "_", daty[i], ".nc")
             ncpath <- file.path(outdir, ncfile)
-            nc <- nc_create(ncpath, outnc)
-            ncvar_put(nc, outnc, tmp)
-            nc_close(nc)
+            ncout <- ncdf4::nc_create(ncpath, outnc)
+            ncdf4::ncvar_put(ncout, outnc, tmp)
+            ncdf4::nc_close(ncout)
         }
 
+        ncdf4::nc_close(ncin)
         return(0)
     })
 
