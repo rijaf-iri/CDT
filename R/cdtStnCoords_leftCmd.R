56594d6a3651bf2dc7f6172d7e6e2792ef943ec6
rijaf-iri <rijaf@iri.columbia.edu>
2023-03-07 16:33:13 +0000

    handle warning

--- R/cdtStnCoords_leftCmd.R
+++ R/cdtStnCoords_leftCmd.R
@@ -177,7 +177,10 @@ StnChkCoordsPanelCmd <- function(){
                 {
                     StnChkCoordsProcs(GeneralParameters)
                 },
-                warning = function(w) warningFun(w),
+                warning = function(w){
+                    warningFun(w)
+                    return(0)
+                },
                 error = function(e) errorFun(e),
                 finally = {
                     tkconfigure(.cdtEnv$tcl$main$win, cursor = '')
